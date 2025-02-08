5e1932a00c0136104389a12d42f1b9b80219520c
Reus Rivera <andreus.jophiel.rivera@gmail.com>
2025-02-08 13:38:04 +0800

    Version 0.0.1

---
+++ QuoteCalculator/Context/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuoteCalculator.Models;
+
+namespace QuoteCalculator.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BorrowerModel> Borrowers { get; set; }
+    }
+}
--- QuoteCalculator/Program.cs
+++ QuoteCalculator/Program.cs
@@ -1,11 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using QuoteCalculator.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Connection Strings:
+string? defaultConnectionString = builder.Configuration.GetConnectionString("DefaultConnectionString");
 
+// Add services to the container.
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// DBContext:
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(defaultConnectionString));
 
 var app = builder.Build();
 
