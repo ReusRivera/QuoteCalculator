a193b280425006274c90aee6d192483e047624cc
Reus Rivera <andreus.jophiel.rivera@gmail.com>
2025-02-15 18:41:50 +0800

    Updated Borrower model's DateOfBirth data type

--- QuoteCalculator.Domain/Models/BorrowerModel.cs
+++ QuoteCalculator.Domain/Models/BorrowerModel.cs
@@ -6,7 +6,7 @@
         public required string Title { get; set; }
         public required string FirstName { get; set; }
         public required string LastName { get; set; }
-        public DateOnly DateOfBirth { get; set; }
+        public DateTime DateOfBirth { get; set; }
         public required string Mobile { get; set; }
         public required string Email { get; set; }
 
