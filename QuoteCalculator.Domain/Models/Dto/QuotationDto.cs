db31995370199a371a9a7940bcd5de76db75e54d
Reus Rivera <andreus.jophiel.rivera@gmail.com>
2025-02-15 15:01:49 +0800

    Updated QuotationDto model

--- QuoteCalculator.Domain/Models/Dto/QuotationDto.cs
+++ QuoteCalculator.Domain/Models/Dto/QuotationDto.cs
@@ -1,14 +1,29 @@
-﻿namespace QuoteCalculator.Domain.Models.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QuoteCalculator.Domain.Models.Dto
 {
     public class QuotationDto
     {
         public int AmountRequired { get; set; }
         public int Term { get; set; }
-        public required string Title { get; set; }
-        public required string FirstName { get; set; }
-        public required string LastName { get; set; }
-        public DateOnly DateOfBirth { get; set; }
-        public required string Mobile { get; set; }
-        public required string Email { get; set; }
+
+        [Required]
+        [StringLength(10, ErrorMessage = "Title length can't be more than 10.")]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(20, ErrorMessage = "First Name length can't be more than 20.")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(20, ErrorMessage = "Last Name length can't be more than 20.")]
+        public string LastName { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string Mobile { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
     }
 }
